04cf4b563696b41e379f298d08176c01b9691bf5
GitAdmin <example@domain.com>
2018-03-07 16:13:05 -0500

    Initial Commit

---
+++ FranciscoRedditObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FranciscoRedditObjC
+//
+//  Created by Admin on 3/7/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FranciscoRedditObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FranciscoRedditObjC
+//
+//  Created by Admin on 3/7/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
